bf15b0eae42f53648c5e2e4feb6b5e5f456d595e
Joren Van Onder <joren.vanonder@gmail.com>
2018-07-07 21:24:58 -0700

    One line merges with conflicts

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,26 @@
 #![allow(unused_variables)]
 
+static CONFLICT_MARKER_OPEN: &str = "<<<<<<<";
+static CONFLICT_MARKER_MID: &str = "=======";
+static CONFLICT_MARKER_CLOSE: &str = ">>>>>>>";
+
+pub fn format_conflict(a: &str, b: &str) -> String {
+    String::from(format!(
+        "{}
+{}
+{}
+{}
+{}",
+        CONFLICT_MARKER_OPEN, a, CONFLICT_MARKER_MID, b, CONFLICT_MARKER_CLOSE
+    ))
+}
+
 pub fn merge_3_way(parent: &str, a: &str, b: &str) -> String {
     let a_modified = !(parent == a);
     let b_modified = !(parent == b);
 
     if a_modified && b_modified {
-        // todo return conflict
-        String::from("conflict")
+        format_conflict(a, b)
     } else if a_modified {
         String::from(a)
     } else if b_modified {
--- tests/git-merge-xml.rs
+++ tests/git-merge-xml.rs
@@ -3,21 +3,29 @@ extern crate git_merge_xml;
 use git_merge_xml::*;
 
 #[test]
-fn test_empty_merge() {
+fn test_one_line_empty_merge() {
     assert_eq!(merge_3_way("", "", ""), "");
 }
 
 #[test]
-fn test_no_change_merge() {
+fn test_one_line_no_change_merge() {
     assert_eq!(merge_3_way("mouse", "mouse", "mouse"), "mouse");
 }
 
 #[test]
-fn test_non_conflicting_merge_1() {
+fn test_one_line_non_conflicting_merge_1() {
     assert_eq!(merge_3_way("", "mouse", ""), "mouse");
 }
 
 #[test]
-fn test_non_conflicting_merge_2() {
+fn test_one_line_non_conflicting_merge_2() {
     assert_eq!(merge_3_way("mouse", "cat", "mouse"), "cat");
+}
+
+#[test]
+fn test_one_line_conflict_1() {
+    let parent = "mouse";
+    let a = "cat";
+    let b = "dog";
+    assert_eq!(merge_3_way(parent, a, b), format_conflict(a, b));
 }
